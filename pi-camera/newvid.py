ee735fb0a6495c90d1cc94b21444794e79f38fee
MullinLabs <goodbeebo@hotmail.com>
2015-12-04 17:38:37 -0700

    Update newvid.py

--- pi-camera/newvid.py
+++ pi-camera/newvid.py
@@ -1,5 +1,6 @@
 # This script uses raspivid to take a video converts it with MP4 box and plays it on a connected display
 import os
-os.system("mkdir /tmp/stream")
-os.system("raspistill --nopreview -w 640 -h 480 -q 5 -o /tmp/stream/pic.jpg -tl 100 -t 9999999 -th 0:0:0 &")
+os.system("raspivid -o myvid.h264 -t 60000")
+os.system("MP4Box -fps 30 -add myvid.h264 myvid.mp4")
+os.system("rm myvid.h264")
 os.system("omxplayer -p -o hdmi myvid.mp4")
